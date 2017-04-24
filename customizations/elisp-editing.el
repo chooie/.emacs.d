2b755e865071318031a902270a8266d4d2f468c7
Charlie Hebert <charlie.hebert92@gmail.com>
2017-04-23 20:55:57 -0600

    Remove smartparens stuff

--- customizations/elisp-editing.el
+++ customizations/elisp-editing.el
@@ -1,14 +1,3 @@
-;; Smartparens
-;; (require 'smartparens-config)
-;; (add-hook 'emacs-lisp-mode-hook       #'smartparens-strict-mode)
-;; (add-hook 'eval-expression-minibuffer-setup-hook #'smartparens-strict-mode)
-;; (add-hook 'ielm-mode-hook             #'smartparens-strict-mode)
-;; (add-hook 'lisp-mode-hook             #'smartparens-strict-mode)
-;; (add-hook 'lisp-interaction-mode-hook #'smartparens-strict-mode)
-;; (add-hook 'scheme-mode-hook           #'smartparens-strict-mode)
-;; (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
-;; (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
-
 ;; eldoc-mode shows documentation in the minibuffer when writing code
 ;; http://www.emacswiki.org/emacs/ElDoc
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
